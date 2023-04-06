9039525255a510689f82cf1f16f4a57a6ad0daa8
“MauSoC” <soriano.m1998@gmail.com>
2023-04-05 23:21:24 -0600

    Ejercicio 1: Resuelto

---
+++ Semana 1/Ejercicio 1/Ejercicio_1.py
@@ -0,0 +1,11 @@
+###	1.	Crear un programa que solicite al usuario su nombre 
+### y su edad, y luego imprima un mensaje de bienvenida con esa información.
+
+def saludo (nombre=None, edad=None):
+    return f'Hola {nombre}, gracias por unirte al programa tu edad es: {edad}.'
+
+
+if __name__ == "__main__":
+    nombre = input('Ingresa tu nombre: ')
+    edad = int(input('ingresa tu edad: '))
+    print(saludo(nombre, edad))
