efb3fff1f9e14777cef15398a9b42cc5ae98b0a0
“MauSoC” <soriano.m1998@gmail.com>
2023-04-05 23:43:35 -0600

    Ejercicio 2 Resuelto

---
+++ Semana 1/Ejercicio 2/Ejercicio_2.py
@@ -0,0 +1,11 @@
+###	2.	Crear un programa que solicite al usuario una cadena de caracteres y luego imprima la cadena al revés.
+
+def CadenaRe(cadena):
+    #utilizamos la sintaxis [::-1] para invertir la cadena y la retornamos directamente con ayuda del fstring
+    return f'{cadena[::-1]}'
+
+if __name__ == "__main__":
+    #Pedimos la cadena al usuario
+    cadena = input('Ingresa una cadena: ')
+    #llamamos la funcion y mandamos la cadena para imprimir el resultado
+    print(CadenaRe(cadena))
